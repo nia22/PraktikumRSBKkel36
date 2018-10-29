75e9094a935d0dbfb4168b75a459c3ff5db35ed1
unknown <nia22>
2018-10-29 11:27:13 +0700

    upload pertama

---
+++ src/snapapp36/SnapAPP36.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp36;
+
+/**
+ *
+ * @author BossNia
+ */
+public class SnapAPP36 extends SnapFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapAPP36 a = new SnapAPP36();
+        a.setVisible (true);
+    }
+    
+}
